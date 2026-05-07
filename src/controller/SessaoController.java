349462e4f85fe7f9e169c1b6ac2b4ef0f4c1b9d4
Miguel Turatti Quirino <miguelturattiq17@gmail.com>
2026-05-07 20:55:08 -0300

    Update SessaoController.java

--- src/controller/SessaoController.java
+++ src/controller/SessaoController.java
@@ -1,1 +1,15 @@
+package controller;
+import service.SessaoService;
 
+public class SessaoController {
+    private SessaoService service = new SessaoService();
+
+    public String adicionarPublico(int id, int qtd) {
+        try {
+            service.registrarVenda(id, qtd);
+            return "Sucesso: Público registrado!";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+}
