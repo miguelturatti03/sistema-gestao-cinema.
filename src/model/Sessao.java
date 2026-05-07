d0e74d07ac43a31660fd118caac751887d701a2c
Miguel Turatti Quirino <miguelturattiq17@gmail.com>
2026-05-07 20:54:41 -0300

    Update Sessao.java

--- src/model/Sessao.java
+++ src/model/Sessao.java
@@ -1,1 +1,14 @@
+package model;
 
+public class Sessao {
+    public int id;
+    public int capacidadeCinema;
+    public int publicoAtual;
+    
+    // Construtor simples
+    public Sessao(int id, int capacidade, int atual) {
+        this.id = id;
+        this.capacidadeCinema = capacidade;
+        this.publicoAtual = atual;
+    }
+}
