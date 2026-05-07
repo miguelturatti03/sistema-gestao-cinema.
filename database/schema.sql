f2b43010c9c6904674a916d6e9e200870b3e95e5
Miguel Turatti Quirino <miguelturattiq17@gmail.com>
2026-05-07 20:45:50 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,22 @@
-CREATE TABLE Cinema;
-CREATE TABLE Filme;
-CREATE TABLE Sessao;
+CREATE TABLE IF NOT EXISTS cinemas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    capacidade INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS filmes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    duracao INTEGER NOT NULL,
+    genero TEXT
+);
+
+CREATE TABLE IF NOT EXISTS sessoes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_filme INTEGER,
+    id_cinema INTEGER,
+    horario DATETIME,
+    publico_atual INTEGER DEFAULT 0,
+    FOREIGN KEY (id_filme) REFERENCES filmes(id),
+    FOREIGN KEY (id_cinema) REFERENCES cinemas(id)
+);
