c44869551c4dee990c59b9f75e476b912439dad4
Miguel Turatti Quirino <miguelturattiq17@gmail.com>
2026-05-07 21:05:30 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -22,3 +22,8 @@ CREATE TABLE IF NOT EXISTS sessoes (
     FOREIGN KEY (id_filme) REFERENCES filmes(id),
     FOREIGN KEY (id_cinema) REFERENCES cinemas(id)
 );
+
+-- Exemplo de dados para teste no final do schema.sql
+INSERT INTO cinemas (nome, capacidade) VALUES ('Cine Centro', 100);
+INSERT INTO filmes (titulo, duracao, genero, diretor) VALUES ('Interstellar', 169, 'Ficção', 'Christopher Nolan');
+INSERT INTO sessoes (id_filme, id_cinema, horario, publico_atual) VALUES (1, 1, '2023-12-01 19:00:00', 0);
